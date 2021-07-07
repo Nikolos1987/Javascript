f2784340b468e889473035c6ec7976a67b790450
archer1987 <nikolos04051987@gmail.com>
2021-07-07 15:21:42 +0700

    д.з1

---
+++ Java.js
@@ -0,0 +1,8 @@
+let degree= +prompt("введите температуру в градусах Цельсия");
+console.log(degree);
+let celsium= (9 / 5) * degree + 32;
+console.log(celsium);
+alert("температура по Фаренгейту= " +celsium+" градусов Фаренгейта");
+let name=prompt("введите имя");
+let user=name;
+document.write("здравствуйте! "+user);
