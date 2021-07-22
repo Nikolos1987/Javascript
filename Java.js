5946f3bac7691833996ed6d694f538331096cbe4
archer1987 <nikolos04051987@gmail.com>
2021-07-22 20:27:22 +0700

    Update Java.js

--- Java.js
+++ Java.js
@@ -1,8 +1,52 @@
-let degree= +prompt("введите температуру в градусах Цельсия");
-console.log(degree);
-let celsium= (9 / 5) * degree + 32;
-console.log(celsium);
-alert("температура по Фаренгейту= " +celsium+" градусов Фаренгейта");
-let name=prompt("введите имя");
-let user=name;
-document.write("здравствуйте! "+user);
+let str = prompt("введите число 0 до 999"); 
+  let arr = str.split('');
+function makeUser(hunder,tens,units) {  
+if (str<10){
+  arr.unshift(0,0)
+}
+if(str<100&&str>=10){
+  arr.unshift(0)
+}
+  if(str>999){
+ let alpha={}
+  return alpha 
+}
+  return {
+    hunder: hunder,
+    tens: tens,
+    units:units,    
+  };
+}
+let user = makeUser(+arr[0], +arr[1],+arr[2]);
+console.log(user)
+
+
+
+let basket={
+  shirts:{
+    price:1234,
+    color:"black",
+    size:'l',
+    quntity:3
+  },
+  jeans:{
+    price: 5000,
+    color:"blue",
+  size:"32",
+  quntity:2
+  },
+ 
+}
+function basketSumm(){
+     let summ=0   
+ for(let key in basket) {  
+  summ+=(basket[key].price) 
+   }
+   return summ
+  }
+let summ=basketSumm()
+console.log(summ)
+ 
+ 
+
+
